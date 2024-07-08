405394f8025af4d0d5d08ec3121d41e3f13e60a5
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-07-08 20:30:57 +0100

    weather api

---
+++ weather_ap/script.js
@@ -0,0 +1,6 @@
+const api_key = "514d525ecb9f03cddfeb25a0b84d1166";
+
+const weatherDataEl = document.getElementById("weather-data");
+const cityInputEl = document.getElementById("city-input");
+
+const formEl = document.querySelector("form")
