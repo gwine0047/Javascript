4d4d7689e15c8ce54e49af16b6e2dee5dd9b7940
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-07-09 07:42:03 +0100

    move script position in html file

--- weather_ap/script.js
+++ weather_ap/script.js
@@ -2,5 +2,18 @@ const api_key = "514d525ecb9f03cddfeb25a0b84d1166";
 
 const weatherDataEl = document.getElementById("weather-data");
 const cityInputEl = document.getElementById("city-input");
+const formEl = document.querySelector("form");
 
-const formEl = document.querySelector("form")
+if (formEl && cityInputEl && weatherDataEl) {
+    formEl.addEventListener("submit", (event) => {
+        // prevent refresh when submit button is hit
+        event.preventDefault();
+
+        const cityValue = cityInputEl.value;
+        console.log(cityValue);
+    });
+}
+
+else {
+    console.error('One or more element are mising');
+}
