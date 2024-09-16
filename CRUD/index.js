b005d8b9ed0131165879d9d75f82085c65f8d667
Okoeguale Godwin <godwindavies2@gmail.com>
2024-09-16 15:21:33 +0100

    CRUD project

--- CRUD/index.js
+++ CRUD/index.js
@@ -32,7 +32,7 @@ app.get('/api/products/:id', async(req, res) => {
 app.post('/api/products', async (req, res) =>{
     try {
         const product = await Product.create(req.body);
-        res.status(200).json(product);
+        res.status(201).json(product);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
